04632dfaaeaba6728c2cb28cc0988794a4f5ca71
Thomas Witt <thomas@infopark.de>
2008-04-09 09:11:20 +0000

    gemspec überarbeitet
    git-svn-id: https://support.infopark.de/ssvn/Kunden/Infopark/mysql_blob_streaming/trunk@9918 0d717532-ebf7-0310-8196-8275e369f051

--- gemspec.rb
+++ gemspec.rb
@@ -1,17 +1,25 @@
 SPEC = Gem::Specification.new do |spec|
 
-  # Folgende Zeile bitte löschen, wenn sie nicht mehr aktuell ist!
-  puts "\n\33[33mFIXME: Spezifikation ist noch nicht vollständig!\e[0m\n"
-
   spec.name = 'mysql_blob_streaming'
   spec.version = '1.0.0'
-  spec.summary = 'A blob streaming extension for native Ruby-MySQL adapter'
+  spec.summary = 'A blob streaming extension for the native Ruby-MySQL adapter'
   spec.author = 'Infopark AG'
-  spec.homepage = 'http://www.infopark.com'
-  spec.email = 'support@infopark.com'
+  spec.homepage = 'http://www.infopark.de/'
+  spec.email = 'info@infopark.de'
+  spec.requirements << 'Infopark Rails Connector (RC)'
+  spec.description = <<-EOF
+    This GEM is required when using the Infopark Rails Connector (RC) in
+    conjunction with MySQL. It has to be installed on all servers on
+    which the RC is running.
+
+    This GEM may only be used in conjunction with a valid license of the
+    Infopark Rails Connector (RC).
+
+    (c) 2008 Infopark AG. All rights reserved.
+  EOF
 
   spec.add_dependency('mysql', '>=2.7')
-  spec.required_ruby_version = '>=1.8.5'
+  spec.required_ruby_version = '>=1.8.6'
 
   spec.files = %w|mysql_blob_streaming.so README|
   spec.require_path = '.'
